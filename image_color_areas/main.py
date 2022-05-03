34f5d48eb70a6ef69233a6c3bf2034904fbb7671
denis <denissamoylovv+git@gmail.com>
2022-05-04 02:16:27 +0300

    added decomposing to set amount of gray grades

--- image_color_areas/main.py
+++ image_color_areas/main.py
@@ -5,56 +5,77 @@ import PIL.Image
 
 
 def import_image(image_path):
-	"""
-	import image from path
-	"""
 	img = PIL.Image.open(image_path)
 	img = np.array(img)
+	if img.shape[2]==3:
+		img=np.append(img,np.ones((img.shape[0],img.shape[1],1),dtype=np.uint8)*255,axis=2)
 	return img
 
 
 
-
-
 def get_quantity_of_each_color(image):
-	"""
-	get quantity of each color in image
-	"""
+	quantity_of_colors=np.zeros((grades),dtype=np.uint8)
+
+	# for i,row in enumerate(image):
+	# 	for j,pix in enumerate(row):
+	# 		quantity_of_colors[pix]+=1
+
 	quantity_of_each_color = np.bincount(image.flatten())
 	return quantity_of_each_color
 
 
+def rgb_to_luminance(img,grades=256):
 
-img=import_image('___files/3.png')
+	lum=img@np.array((0.05,0.05,0.9,0))
+	lum=lum*(grades-1)/256
+	return lum.round().astype(np.uint8)
 
-img=img@np.array((0.2126,0.7152,0.0722))
-img=img.round().astype(int)
+def export_image(img,image_path):
+	img=PIL.Image.fromarray(img,mode='RGBA')
+	img.save(image_path)
 
-colors=get_quantity_of_each_color(img)
-
-# split array by 0 ellements
-colors=np.split(colors,np.where(colors==0)[0])
-
-print(f"{colors}")
-
-
+grades=16
+palet=[[255,0,0],[0,255,0],[0,0,255],[255,255,0],[0,255,255],[255,0,255],[127,0,255],[255,127,0],[255,255,127],[0,255,127],[127,255,0],[0,127,255],[127,0,0],[0,127,0],[0,0,127],[127,127,0],[0,127,127],[127,0,127],[127,127,127]]
 
 
+def main():
+
+	img=import_image('___files/img1.png')
+	img_lum=rgb_to_luminance(img,grades=grades)
+
+	colors=get_quantity_of_each_color(img_lum)
+
+	colors_temp=colors.flatten()
+	colors_temp.sort()
+	n_max=colors_temp[-4]
+	for i in colors.shape:
+		if colors[i-1]<n_max/2:
+			colors[i]=None
+	
+	quantity_of_colors=np.count_nonzero(colors)
+
+	img_layer=np.zeros((img.shape[0],img.shape[1],4),dtype=np.uint8)
 
 
 
-# image_gs = np.zeros((image.shape[0], image.shape[1]))
+	for i,row in enumerate(img_lum):
+		for j,pix in enumerate(row):
+			if colors[pix] is not None:
+				img_layer[i,j]=palet[pix]+[255]
+			else:
+				img_layer[i,j]=(255,255,255,255)
+				img_layer[i,j]=(0,0,0,255)
+			pass
 
-# # image_gs[i][j]=pix
 
-# # # numpy array to image
-# image = PIL.Image.fromarray(image_gs)
 
-# # convert to rgb 
-# image = image.convert('RGB')
+	export_image(img_layer,'___files/img1_layer.png')
+	
 
-# # save image
-# image.save('_files/3_hsv.png')
+
+
+if __name__ == '__main__':
+	main()
 
 
 
